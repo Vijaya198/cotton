3ef435157f67fdb4d54e44d0d2ecbd24b826420c
umadevi-dharmaraj <59370692+umadevi-dharmaraj@users.noreply.github.com>
2020-03-03 21:29:19 +0530

    removed trade_information from cot_sandhai2

--- trade_information/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import trade_information
-# Register your models here.
-admin.site.register(trade_information)
--- trade_information/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TradeInformationConfig(AppConfig):
-    name = 'trade_information'
