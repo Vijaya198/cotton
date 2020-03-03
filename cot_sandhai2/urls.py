3addd660a0aecb837dab250e210091af591b4dc0
umadevi-dharmaraj <59370692+umadevi-dharmaraj@users.noreply.github.com>
2020-03-03 13:28:02 +0530

    added accountlogin and traderform to project cot_sandhai2

---
+++ cot_sandhai2/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('account_login.urls')),
+
+    path('', include('trade_information.urls')),
+    # path('account_login/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+]
---
+++ vendors/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VedorsConfig(AppConfig):
+    name = 'vendors'
